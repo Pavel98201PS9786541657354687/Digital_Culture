01e21f11807ee6ac1789fc11d4405b8f4368058c
Pavel <pavkapotemkin@yandex.ru>
2024-12-26 09:40:39 +0300

    Изменил Content-Type на text/html

---
+++ back/backend/middleware.py
@@ -0,0 +1,8 @@
+class ContentTypeMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        response['Content-Type'] = 'text/html'
+        return response
