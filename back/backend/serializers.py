0c34eeb851ba37e62b35a6ceb4231dda8bf004ad
Torino59 <49292908+Pavel98201PS9786541657354687@users.noreply.github.com>
2025-01-11 22:08:33 +0300

    Правки по бэку

--- back/backend/serializers.py
+++ back/backend/serializers.py
@@ -25,6 +25,40 @@ class blocksSerializer(serializers.ModelSerializer):
 
 # Метод для вывода проектных файлов
 class projectsFilesSerializer(serializers.ModelSerializer):
+    title = serializers.SerializerMethodField()
+    description = serializers.SerializerMethodField()
+    title_en = serializers.SerializerMethodField()
+    description_en = serializers.SerializerMethodField()
+    
     class Meta:
         model = projectsFilesModel
-        fields = '__all__'
+        fields = ('id', 'projectsId', 'title','title_en',
+                  'description','description_en', 'name', 'fileName',
+                  'format', 'dateCreated', 'dateUpdate', 'weight')
+        
+    def get_title(self, obj):
+        return "Title"
+    
+    def get_description(self, obj):
+        return "Description"
+        
+    def get_title_en(self, obj):
+        return "Title_en"
+    
+    def get_description_en(self, obj):
+        return "Description_en"
+    
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        projectsId = representation['projectsId']
+        # print(instance)
+        # Получаем заголовок и описание
+        title = moviesModel.objects.filter(id = projectsId).values_list("title")[0][0]
+        description = moviesModel.objects.filter(id = projectsId).values_list("description")[0][0]
+        title_en = moviesModel.objects.filter(id = projectsId).values_list("title_en")[0][0]
+        description_en = moviesModel.objects.filter(id = projectsId).values_list("description_en")[0][0]
+        representation['title'] = title
+        representation['title_en'] = title_en
+        representation['description'] = description
+        representation['description_en'] = description_en
+        return representation 
