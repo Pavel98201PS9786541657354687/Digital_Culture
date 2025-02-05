2d70e10aa15b1f1b401e7af671dab2de0e3b7437
Pavel <pavkapotemkin@yandex.ru>
2025-02-05 19:20:05 +0300

    for deploy

--- back/digitalCulture/settings.py
+++ back/digitalCulture/settings.py
@@ -107,19 +107,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
 
 MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static') 
-
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
 STATIC_URL = '/static/'
-# STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     BASE_DIR / "static",
-# ]
-# STATIC_ROOT = BASE_DIR / "staticfiles"
-
-
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static') 
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
